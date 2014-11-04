7fd86f3c9c02617ab015ccf4ff1e71ab081aaf87
Oakland Peters <oakland.peters@gmail.com>
2014-11-04 12:03:52 -0500

    Experimenting with git features.

--- getcounty/test/final_processing.py
+++ getcounty/test/final_processing.py
@@ -29,16 +29,22 @@ def process_file(infile):
 
 def count_unfilled_counties(filepath):
     return processing.count_unfilled(filepath, 'Home of Record County')
-    
-    
 
-if __name__ == "__main__":
-    
+def run_task():
+    """Run task on pre-defined set of files."""
+    # testdir is the directory containing final_processing.py
+    testdir = os.path.split(os.path.abspath(__file__))[0]
+    ticketdir = os.path.join(testdir, '..', 'ticket-files')
     infiles = [
-        os.path.join('..', 'ticket-files', 'ONDNames_of_Fallen__1___1_.csv'),
-        os.path.join('..', 'ticket-files', 'OIFNames_of_Fallen__1___1_.csv'),
-        os.path.join('..', 'ticket-files', 'OEFNames_of_Fallen.csv')
+        os.path.join(ticketdir, 'ONDNames_of_Fallen__1___1_.csv'),
+        os.path.join(ticketdir, 'OIFNames_of_Fallen__1___1_.csv'),
+        os.path.join(ticketdir, 'OEFNames_of_Fallen.csv')
     ]
     
     for infile in infiles:
         process_file(infile)
+    
+
+if __name__ == "__main__":
+    run_task()
+
